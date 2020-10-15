7ee9167cfca5171025352e99c63feb523402d46f
Dmateosca <mateoscano.david@gmail.com>
2020-10-15 22:13:17 +0200

    Subida de los cambios de training

--- backend_gym/controllers/training.js
+++ backend_gym/controllers/training.js
@@ -15,61 +15,66 @@ var TrainingController = {
       { nickname: nickname },
       { dias: { $exists: day } },
       (err, trainingTable) => {
-        if(err) return res.status(500).send("Ha ocurrido un error al buscar el entrenamiento");
+        if (err)
+          return res
+            .status(500)
+            .send("Ha ocurrido un error al buscar el entrenamiento");
         return res.status(200).send({
           training: trainingTable,
         });
       }
     );
   },
-  createTraining: function(req,res){
-
+  createTraining: function (req, res) {
     var body = req.body;
     var nickname = body.nickname;
     var userId = body.idUsuario;
     var tabla = new Tabla();
-    
-    body.ejercicios.forEach(element => {
+
+    body.ejercicios.forEach((element) => {
       var ejercicios = {
         ejercicio: {
-          nombre: element.ejercicio.nombre
-          
+          nombre: element.ejercicio.nombre,
         },
-        series : element.series.map( serie => {
+        series: element.series.map((serie) => {
           return {
             peso: serie.peso,
-            repeticiones: serie.repeticiones
-          }
-
-        }
-          
-        )
-      }
-      tabla.ejercicios.push(ejercicios)
-      
+            repeticiones: serie.repeticiones,
+          };
+        }),
+      };
+      tabla.ejercicios.push(ejercicios);
     });
 
     tabla.usuario = nickname;
     tabla.idUsuario = userId;
     tabla.dias = body.dias;
 
-    tabla.save((err,trainingInsert) => {
+    tabla.save((err, trainingInsert) => {
+      if (err)
+        return res
+          .status(500)
+          .send("No se ha podido almacenar el entrenamiento");
 
-      if(err) return res.status(500).send("No se ha podido almacenar el entrenamiento");
+      if (!trainingInsert)
+        return res.status(400).send("Entramiento no almacenado");
 
-      if (!trainingInsert) return res.status(400).send("Entramiento no almacenado");
-
-      User.findOneAndUpdate({nickname: nickname}, {$push: {
-        entrenamientos: trainingInsert._id
-      }},(err,updateTraining) =>{
-        if(err) console.log(err);
-      });
+      User.findOneAndUpdate(
+        { nickname: nickname },
+        {
+          $push: {
+            entrenamientos: trainingInsert._id,
+          },
+        },
+        (err, updatedTraining) => {
+          if (err) console.log(err);
+        }
+      );
       return res.status(200).send({
-        message: "Se ha insertado correctamente"
+        message: "Se ha insertado correctamente",
       });
-    })
-  }
+    });
+  },
 };
-
 
 module.exports = TrainingController;
